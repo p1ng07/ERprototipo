0fd974e8b93ff9d6d4cd229726611c7210730a53
VitorVieira20 <2078320@student.uma.pt>
2023-12-08 21:34:59 +0000

    Added: Now the admin can see the images from the pass request

--- routes/getPass.js
+++ routes/getPass.js
@@ -1,25 +1,48 @@
 var express = require("express");
 var router = express.Router();
+var multer = require("multer");
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../public/uploads/')); //Caminho onde as imagens serão armazenadas
+    },
+    filename: function (req, file, cb) {
+        //Função para tratar os caracteres especiais
+        cb(null, Date.now() + '-' + encodeURIComponent(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
 
 /* GET Pedir passe page. */
 router.get("/", function (req, res, next) {
     res.render("getPass")
 });
 
-router.post("/", function (req, res, next) {
-    const formData = {
-        nomeMembroUMa: req.body.nomeMembroUMa,
-        numberMembroUMa: req.body.numberMembroUMa,
-        CCMembroUMa: req.body.CCMembroUMa,
-        cartaoMembroUMa: req.body.cartaoMembroUMa,
-        tipoPasse: req.body.tipoPasse,
-        comprovativoMorada: req.body.comprovativoMorada,
-        emitted: false
-    }
+router.post("/", upload.fields([
+    { name: 'CCMembroUMa', maxCount: 1 },
+    { name: 'cartaoMembroUMa', maxCount: 1 },
+    { name: 'comprovativoMorada', maxCount: 1 }
+]), function (req, res, next) {
+    try {
+        const formData = {
+            nomeMembroUMa: req.body.nomeMembroUMa,
+            numberMembroUMa: req.body.numberMembroUMa,
+            CCMembroUMa: '/uploads/' + req.files['CCMembroUMa'][0].filename,
+            cartaoMembroUMa: '/uploads/' + req.files['cartaoMembroUMa'][0].filename,
+            tipoPasse: req.body.tipoPasse,
+            comprovativoMorada: req.files['comprovativoMorada'] ? '/uploads/' + req.files['comprovativoMorada'][0].filename : null,
+            emitted: false
+        };
 
-    req.session.studentFormData = req.session.studentFormData || []
-    req.session.studentFormData.push(formData);
-    res.redirect('/')
-})
+        req.session.studentFormData = req.session.studentFormData || [];
+        req.session.studentFormData.push(formData);
+        res.redirect('/');
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 module.exports = router;
