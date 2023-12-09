1df7902f4a6657007cefc6973c7225c58e7def04
Not-Hard <juliopitno@gmail.com>
2023-12-09 23:23:45 +0000

    Added: Registo Validation

--- routes/registo.js
+++ routes/registo.js
@@ -2,16 +2,44 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.render("registo", { error_check_email: false});
+  res.render("registo", { error_check_email: false, error_check_number: false, error_check_password: false, error_check_confirma_password: false});
 }); 
 
 router.post("/", function (req, res, next) {
-    // Verifica se ocorreu um erro na criação do email
-    let error_check_email = true;
+    
+  let error_check_email = true;
+    
+  let error_check_number = true;
 
-    if(Object.keys(req.body.check_email).length === 0) {
-        res.render("registo", {error_check_email: error_check_email});
-    }   
+  let error_check_password = true;
+
+  let error_check_confirma_password = true;
+
+   // Verifica se o email não está vazio
+  if(Object.keys(req.body.check_email).length !== 0) {
+    error_check_email = false;
+  } 
+
+  // Verifica se o numero não está vazio
+  if(Object.keys(req.body.check_numero).length !== 0) {
+    error_check_number = false;
+  } 
+
+  // Verifica se a password não está vazia
+  if(Object.keys(req.body.check_password).length !== 0) {
+    error_check_password = false;
+  }   
+  
+  // Verifica se o Confirma password não está vazio
+  if(Object.keys(req.body.check_confirma_password).length !== 0) {
+    error_check_confirma_password = false;
+  } 
+
+  //Verifica se está algum campo vazio
+  if(error_check_number == true || error_check_email == true || error_check_password == true || error_check_confirma_password == true){
+    res.render("registo", {error_check_number: error_check_number, error_check_email:error_check_email, error_check_password: error_check_password, error_check_confirma_password:error_check_confirma_password});
+  }
+  
 });
 
 
