057d7b8136a0c1c3c93ec8854d8470be8a2c7f22
p1ng07 <franciscoppontes369@gmail.com>
2023-12-14 11:38:35 +0000

    Changed: Mudar horarios para home

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("home", { title: "Home" });
+  res.render("home", { title: "Home", carreira_error: false });
 });
 
-router.post("/", function (req, res, next) {
-  res.render("home", { title: "Home" });
+router.post("/visualizar-horario", function (req, res, next) {
+  let carreira_error = false;
+  const horarioSelecionado = req.body.horario;
+
+  if (horarioSelecionado == "") {
+    carreira_error = true;
+  }
+
+  if (carreira_error) {
+    res.render("horarios", { carreira_error });
+  } else {
+    const horarioPath = path.join(
+      __dirname,
+      "../public/horarios",
+      `${horarioSelecionado}.pdf`
+    );
+    res.sendFile(horarioPath);
+  }
 });
 
 module.exports = router;
